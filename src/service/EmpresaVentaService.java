803b29109b1cdddd26ca1d068560bf18703f9814
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-15 11:25:50 -0500

    commit - EmpresaVentaService / .

---
+++ src/service/EmpresaVentaService.java
@@ -0,0 +1,15 @@
+package service;
+
+import dao.EmpresaVentaDAO;
+import model.EmpresaVenta;
+
+import java.util.List;
+
+public class EmpresaVentaService {
+
+    private EmpresaVentaDAO dao = new EmpresaVentaDAO();
+
+    public List<EmpresaVenta> obtenerEmpresaVentas() {
+        return dao.listar();
+    }
+}   
--- src/service/InfoClienteService.java
+++ src/service/InfoClienteService.java
@@ -12,4 +12,4 @@ public class InfoClienteService {
     public List<InfoCliente> obtenerInfoClientes() {
         return dao.listar();
     }
-}   
+}
