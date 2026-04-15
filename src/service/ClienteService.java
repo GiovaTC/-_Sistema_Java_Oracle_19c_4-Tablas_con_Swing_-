f95ba32ac69bd3a3f4220ab918f3551bfd66b266
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-15 10:57:46 -0500

    commit - ClienteService .

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,4 +1,15 @@
 package service;
 
+import dao.ClienteDAO;
+import model.Cliente;
+
+import java.util.List;
+
 public class ClienteService {
-}
+
+    private ClienteDAO dao = new ClienteDAO();
+
+    public List<Cliente> obtenerClientes() {
+        return dao.listar();
+    }
+}   
