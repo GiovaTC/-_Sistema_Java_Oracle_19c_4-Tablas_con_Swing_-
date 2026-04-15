0ec5ee2b92e36baa9b406683078973c0169128ac
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-15 10:54:01 -0500

    commit - EmpresaVentaDAO / .

---
+++ src/dao/EmpresaVentaDAO.java
@@ -0,0 +1,36 @@
+package dao;
+
+import database.Conexion;
+import model.EmpresaVenta;
+import model.InfoCliente;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaVentaDAO {
+
+    public List<EmpresaVenta> listar() {
+        List<EmpresaVenta> lista = new ArrayList<>();
+
+        try (Connection conn = Conexion.getConnection();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM EMPRESA_VENTA")) {
+
+            while (rs.next()) {
+                lista.add(new EmpresaVenta(
+                        rs.getInt("ID"),
+                        rs.getString("NOMBRE"),
+                        rs.getString("CIUDAD")
+                ));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+}   
